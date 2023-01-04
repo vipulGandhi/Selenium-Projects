5c2b4784411b830dd36a286183b6b11eda843842
Vipul Gandhi <vipulgandhi10@gmail.com>
2023-01-04 20:24:50 +0530

    Commit
    Commit

---
+++ src/main/java/CustomWait/WaitMechanism.java
@@ -0,0 +1,55 @@
+package CustomWait;
+
+import org.bouncycastle.jcajce.provider.asymmetric.rsa.DigestSignatureSpi.noneRSA;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WaitMechanism
+{
+	public static void main(String[] args)
+	{
+		WebDriver driver = new ChromeDriver();
+		float pollingTime = 0.5f;
+		int waitDution = 10;
+		float timeSpent = 0f;
+		
+		WebElement webElement = null;
+		
+		while (timeSpent < waitDution)
+		{
+			try
+			{
+				webElement = driver.findElement(null);
+				break;
+			}
+			catch (Exception e)
+			{
+
+					try {
+						Thread.sleep((long) pollingTime);
+					} catch (InterruptedException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+			}
+			timeSpent ++;
+		}
+		
+		if (webElement == null)
+		{
+			try {
+				throw new Exception("NoElementFound");
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			
+		}
+	}
+}
+
+// Find the element
+// if the element is not found, wait for polling time
+
+// polling 
